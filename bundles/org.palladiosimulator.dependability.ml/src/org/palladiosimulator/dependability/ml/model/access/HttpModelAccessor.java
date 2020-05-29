88a937c59a656b0b4855c65f4b1fdf2e08f0fc24
Max Scheerer <scheerer@fzi.de>
2020-05-29 08:25:31 +0200

    Converting input data of the http client to json data.

--- bundles/org.palladiosimulator.dependability.ml/src/org/palladiosimulator/dependability/ml/model/access/HttpModelAccessor.java
+++ bundles/org.palladiosimulator.dependability.ml/src/org/palladiosimulator/dependability/ml/model/access/HttpModelAccessor.java
@@ -1,21 +1,23 @@
 package org.palladiosimulator.dependability.ml.model.access;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import java.util.Base64;
 
 import org.palladiosimulator.dependability.ml.exception.DependableMLException;
 import org.palladiosimulator.dependability.ml.model.InputData;
 
 public class HttpModelAccessor implements TrainedModelAccessor<InputData<?>, String> {
 
+	private static final String PREDICTION_ROUTE = "/predict";
 	private static final String SUPPORTED_SCHEME = "http";
 	private static final int TIME_OUT_DURATION = 2;
 
@@ -30,13 +32,16 @@ public class HttpModelAccessor implements TrainedModelAccessor<InputData<?>, Str
 	@Override
 	public void load(URI modelURI) {
 		client = HttpClient.newHttpClient();
-		remoteModelURI = modelURI;
+		remoteModelURI = adjustQueryUri(modelURI);
 	}
 
 	@Override
 	public String query(InputData<?> inputData) {
-		var request = HttpRequest.newBuilder().uri(remoteModelURI).timeout(Duration.ofMinutes(TIME_OUT_DURATION))
-				.POST(toBody(inputData)).build();
+		var request = HttpRequest.newBuilder(remoteModelURI)
+				.header("Content-Type", "application/json")
+				.timeout(Duration.ofMinutes(TIME_OUT_DURATION))
+				.POST(BodyPublishers.ofString(prepareForSending(inputData)))
+				.build();
 
 		try {
 			return client.send(request, BodyHandlers.ofString()).body();
@@ -47,19 +52,45 @@ public class HttpModelAccessor implements TrainedModelAccessor<InputData<?>, Str
 		}
 	}
 
-	private BodyPublisher toBody(InputData<?> inputData) {
+	private URI adjustQueryUri(URI modelURI) {
+		var adjustedUri = modelURI.toString().concat(PREDICTION_ROUTE);
+		try {
+			return new URI(adjustedUri);
+		} catch (URISyntaxException e) {
+			throw DependableMLException.supplierWithMessageAndCause(
+					String.format("The syntax of the adjusted uri %s is not valid.", adjustedUri), e).get();
+		}
+	}
+
+	private String prepareForSending(InputData<?> inputData) {
+		var body = "";
 		var value = inputData.getValue();
+
 		if (value instanceof String) {
-			return BodyPublishers.ofString((String) value);
+			body = (String) value;
 		} else if (value instanceof File) {
-			try {
-				return BodyPublishers.ofFile(((File) value).toPath());
-			} catch (FileNotFoundException e) {
-				DependableMLException.throwWithMessageAndCause("The input data file could not found.", e);
-			}
+			body = encode((File) value);
+		} else {
+			DependableMLException.throwWithMessage("The input data format is not supported.");
 		}
 
-		throw DependableMLException.supplierWithMessage("The input data format is not supported.").get();
+		return jsonify(body);
+	}
+
+	private String encode(File fileToEncode) {
+		String base64Image = "";
+		try (FileInputStream imageInFile = new FileInputStream(fileToEncode)) {
+			byte imageData[] = new byte[(int) fileToEncode.length()];
+			imageInFile.read(imageData);
+			base64Image = Base64.getEncoder().encodeToString(imageData);
+		} catch (IOException e) {
+			DependableMLException.throwWithMessageAndCause("Something went wrong during file encoding.", e);
+		}
+		return base64Image;
+	}
+
+	private String jsonify(String value) {
+		return String.format("{\"value\": %s}", value);
 	}
 
 }
